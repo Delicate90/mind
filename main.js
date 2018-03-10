405257f550e06e719e910d43768c329a3f66a2fd
Delicate <13935111660@139.com>
2018-03-10 11:17:30 +0800

    func for hide

--- main.js
+++ main.js
@@ -7,141 +7,154 @@ $(() => {
 
 const lineWidth = 60; //group 与 block 间距离
 let jsonDepth = 0;
-
-const init = () => {
-    load(()=>{
-        edit();
-        drag();
-    });
-};
-//todo 读取json并渲染
-const load = (callback) => {
-    const json = {
-        blocks: {
+const mindDict = {
+    blocks: [
+        {
             id: 0,
             content: 'root',
-            group: {
-                level: 0,
-                parent: 0,
-                children: [
-                    {
-                        id: 1,
-                        content: 'block-1',
-                        group: {
-                            level: 1,
-                            parent: 1,
-                            children: [
-                                {
-                                    id: 3,
-                                    content: 'block-3',
-                                    group: {
-                                        level: 2,
-                                        parent: 3,
-                                        children: []
-                                    }
-                                },{
-                                    id: 4,
-                                    content: 'block-4',
-                                    group: {
-                                        level: 2,
-                                        parent: 4,
-                                        children: []
-                                    }
-                                }
-                            ]
-                        }
-                    },{
-                        id: 2,
-                        content: 'block-2',
-                        group: {
-                            level: 1,
-                            parent: 2,
-                            children: [
-                                {
-                                    id: 5,
-                                    content: 'block-5',
-                                    group: {
-                                        level: 2,
-                                        parent: 5,
-                                        children: [
-                                            {
-                                                id: 7,
-                                                content: 'block-7',
-                                                group: {
-                                                    level: 3,
-                                                    parent: 7,
-                                                    children: []
-                                                }
-                                            },{
-                                                id: 8,
-                                                content: 'block-8',
-                                                group: {
-                                                    level: 3,
-                                                    parent: 8,
-                                                    children: []
-                                                }
-                                            }
-                                        ]
-                                    }
-                                },{
-                                    id: 6,
-                                    content: 'block-6',
-                                    group: {
-                                        level: 2,
-                                        parent: 6,
-                                        children: []
-                                    }
-                                }
-                            ]
-                        }
-                    }
-                ]
-            }
+            parent: 'root',
+            childHide: false,
+            childHided: false,
+            childLevel: 0
+        }, {
+            id: 1,
+            content: 'block-1',
+            parent: 0,
+            childHide: false,
+            childHided: false,
+            childLevel: 1
+        }, {
+            id: 2,
+            content: 'block-2',
+            parent: 0,
+            childHide: false,
+            childHided: false,
+            childLevel: 1
+        }, {
+            id: 3,
+            content: 'block-3',
+            parent: 1,
+            childHide: false,
+            childHided: false,
+            childLevel: 2
+        }, {
+            id: 4,
+            content: 'block-4',
+            parent: 1,
+            childHide: false,
+            childHided: false,
+            childLevel: 2
+        }, {
+            id: 5,
+            content: 'block-5',
+            parent: 2,
+            childHide: false,
+            childHided: false,
+            childLevel: 2
+        }, {
+            id: 6,
+            content: 'block-6',
+            parent: 2,
+            childHide: false,
+            childHided: false,
+            childLevel: 2
+        }, {
+            id: 7,
+            content: 'block-7',
+            parent: 5,
+            childHide: false,
+            childHided: false,
+            childLevel: 3
+        }, {
+            id: 8,
+            content: 'block-8',
+            parent: 5,
+            childHide: false,
+            childHided: false,
+            childLevel: 3
         }
-    };
-    console.error(new Date().getTime());
+    ]
+};
+const mindJson = {
+    blocks: {}
+};
+
+const init = () => {
+    load(() => {
+        edit();
+        drag();
+        hide();
+    });
+};
+//读取json并渲染
+const load = (callback) => {
     let htmlArray = [];
     let depth = 0;
-    const split = (group,hide) => {
+    const cover = (parentId) => mindDict.blocks.filter((item)=>item.parent === parentId).map((block)=>{
+        return {
+            id: block.id,
+            content: block.content,
+            group: {
+                level: block.childLevel,
+                hide: block.childHide,
+                hided: block.childHided,
+                parent: block.id,
+                children: cover(block.id)
+            }
+        }
+    });
+
+    const split = (group) => {
         let htmlStr = '';
         depth = depth > group.level ? depth : group.level;
-        htmlStr += `<div data-level="${group.level}" data-block-group="${group.parent}" class="mind-group ${hide? 'mind-group-hide' : ''}">`;
-        for(let child of group.children) {
+        htmlStr += `<div data-level="${group.level}" data-block-group="${group.parent}" class="mind-group ${group.hided ? 'mind-group-hide' : ''}">`;
+        for (let childIndex in group.children) {
+            const child = group.children[childIndex];
             htmlStr += `<div class="mind-shell">
                 <div data-block-id="${child.id}" class="mind-block mind-color-primary">
                     <div class="mind-view">
                         <div class="mind-block-content">${child.content}</div>
                     </div>
                     <div class="mind-edit" contenteditable="true">${child.content}</div>
-                    ${group.children.length > 0 ? '<div data-hide="'+group.hide+'" class="mind-block-branch">-</div>' : ''}
+                    ${child.group.children.length > 0 ?
+                '<div data-block-id="' + child.id + '" data-hided="' + (child.group.hided || false) + '" data-hide="' + (child.group.hide || false) + '" class="mind-block-branch ' + (child.group.hide ? 'branch-hide' : '') + '"></div>' : ''}
                 </div>
             </div>`;
-            split(child.group,hide);
+            split(child.group);
         }
         htmlStr += `</div>`;
         htmlArray.push(htmlStr);
     };
+    mindJson.blocks = {
+        id: 0,
+        content: 'root',
+        group: {
+            level: 0,
+            parent: 0,
+            children: cover(0)
+        }
+    };
+
     const rootSplit = (block) => {
         const root = $('.mind-first');
         root.data('block-id', block.id);
         root.find('.mind-block-content').html(block.content);
         root.find('.mind-edit').html(block.content);
-        if(block.group.children.length > 0) root.addClass('mind-block-branch');
     };
-    rootSplit(json.blocks);
-    split(json.blocks.group, json.blocks.group.hide);
+    rootSplit(mindJson.blocks);
+    split(mindJson.blocks.group);
     jsonDepth = depth;
     $('.mind-panel .mind-group').remove();
     $('.mind-panel').append(htmlArray.join(''));
-    callback();
     calc(jsonDepth);
+    callback();
 };
 
 //溯洄计算并添加高度等属性 - 递归
 const calc = (length) => {
     //递归循环group节点
     $('.mind-group[data-level=' + length + ']').each(function () {
-        if($(this).children('.mind-shell').length > 0){
+        if ($(this).children('.mind-shell').length > 0) {
             //获取group父级block
             const parentBlock = $('.mind-block[data-block-id=' + $(this).data('block-group') + ']');
             const firstShellTop = $(this).children('.mind-shell').first().children().position().top;
@@ -159,7 +172,7 @@ const calc = (length) => {
             }
             //设置父级高度
             if ($(this).data('block-group') !== 0 && parentBlock.outerHeight() < $(this).outerHeight()) parentBlock.parent().css("height", $(this).outerHeight() + "px");
-            $(this).data('parent-padding-left',parentBlock.outerWidth() / 2); //传递参数
+            $(this).data('parent-padding-left', parentBlock.outerWidth() / 2); //传递参数
         }
     });
     length--;
@@ -198,18 +211,18 @@ const draw = () => {
         $(this).children('svg').remove();
         $(this).prepend(`<svg xmlns="http://www.w3.org/2000/svg" version="1.1" style="position: absolute;left: 0;top: 0;width: ${$(this).outerWidth()};height: ${$(this).height()};">${linesStr}</svg>`);
     });
-    console.error(new Date().getTime());
 }
-//todo 村入json
+//todo 存入json
 const save = () => {
 
 };
 
 //事件 - 编辑
 const edit = () => {
-    $('.mind-block').off('click');
-    $('.mind-block').off('blur');
-    $('.mind-block').on('click', function () {
+    const blocks = $('.mind-block');
+    blocks.off('click');
+    blocks.off('blur');
+    blocks.on('click', function () {
         if ($(this).hasClass('mind-block-focus')) {
             $(this).find('.mind-edit').addClass('mind-edit-active');
             $(this).find('.mind-edit').focus();
@@ -221,48 +234,94 @@ const edit = () => {
     $('.mind-edit').on('blur', function () {
         $(this).parent().find('.mind-block-content').html($(this).html());
         $(this).removeClass('mind-edit-active');
-        calc(3);
+        calc(jsonDepth);
     });
+};
+
+//事件 - 隐藏
+const hide = () => {
+    const branches = $('.mind-block-branch');
+    $('.mind-group-hide').addClass('mind-group-hide-animate');
+    branches.off('click');
+    branches.on('click', function (e) {
+        e.stopPropagation();
+        const blockId = $(this).data('block-id');
+        const cover = (blockId, bool) => {
+            for(let block of mindDict.blocks){
+                if(block.parent === blockId){
+                    if(!block.childHide) block.childHided = bool;
+                    cover(block.id,bool);
+                }
+            }
+        };
+        const coverSelf = (blockId, bool) => {
+            const blockIndex = mindDict.blocks.findIndex((item)=>item.id === blockId);
+            mindDict.blocks[blockIndex].childHided = bool;
+            mindDict.blocks[blockIndex].childHide = bool;
+        };
+        if ($(this).data('hide')){
+            $(this).data('hide',false);
+            $(this).data('hided',false);
+            $(this).removeClass('branch-hide');
+            coverSelf(blockId,false);
+            cover(blockId,false);
+        }else{
+            $(this).data('hide',true);
+            $(this).data('hided',true);
+            $(this).addClass('branch-hide');
+            coverSelf(blockId,true);
+            cover(blockId,true);
+        }
+        load(() => {
+            edit();
+            drag();
+            hide();
+        });
+    })
 };
 
 //事件 - 拖拽
 const drag = () => {
+    const blocks = $('.mind-block');
     let dragBlock = null;
-    $('.mind-block').off('mousedown');
+    let parentBlock = null;
+    blocks.off('mousedown');
     $(document).off('mouseup');
-    $('.mind-block').on('mousedown',function(ex){
-        if($(this).hasClass('mind-first')){
+    blocks.on('mousedown', function (ex) {
+        if ($(this).hasClass('mind-first')) {
 
-        }else{
-            if($(this).hasClass('mind-block-focus') && !$(this).hasClass('mind-edit-active')){
+        } else {
+            if ($(this).hasClass('mind-block-focus') && !$(this).hasClass('mind-edit-active')) {
                 dragBlock = $(this);
                 $('.mind-drag').html('').append($(this).clone());
                 $('.mind-drag .mind-block').css({
-                    'width': $(this).outerWidth() +'px',
+                    'width': $(this).outerWidth() + 'px',
                     'position': 'absolute',
                     'z-index': '12',
                     'opacity': '0.6'
                 });
-                $(document).on('mousemove',function(e){
+                $(document).on('mousemove', function (e) {
                     const dragBlock = $('.mind-drag .mind-block');
-                    if(dragBlock.length > 0){
+                    if (dragBlock.length > 0) {
                         dragBlock.css({
                             'left': e.pageX - ex.offsetX + 'px',
                             'top': e.pageY - ex.offsetY + 'px',
                         });
                     }
                 });
-                $('.mind-panel .mind-block').on('mouseover',function(){
+                $('.mind-panel .mind-block').on('mouseover', function () {
                     $(this).addClass('mind-block-drag-over');
+                    parentBlock = $(this);
                 });
-                $('.mind-panel .mind-block').on('mouseleave',function(){
+                $('.mind-panel .mind-block').on('mouseleave', function () {
                     $(this).removeClass('mind-block-drag-over');
+                    parentBlock = null;
                 });
             }
         }
     });
-    $(document).on('mouseup',function(){
-        if($('.mind-drag .mind-block').length > 0){
+    $(document).on('mouseup', function () {
+        if ($('.mind-drag .mind-block').length > 0) {
             dragBlock = null;
             $('.mind-drag').html('');
             $(document).off('mousemove');
